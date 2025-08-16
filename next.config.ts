6e4321029a0ed5b616294cb86cf15026a41fac7e
BAD X <badx331@gmail.com>
2025-08-16 09:49:49 +0000

    Images nehi a raha hai

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.promptperfect.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
