d7f96430817152017f2ac3323482e66e562820c3
BAD X <badx331@gmail.com>
2025-08-16 09:40:44 +0000

    Ye buttons fir v 2 2 jaga hai and remove youtuber , influencer text from

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,10 +6,6 @@ const Header = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <a href="#" className="text-2xl font-black tracking-tighter">HYREXVERSE</a>
-          <div className="text-sm font-medium space-x-4">
-            <span>YouTuber</span>
-            <span>Influencer</span>
-          </div>
         </div>
       </div>
     </header>
